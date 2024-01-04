8e6924b5d03e8556550a0966f28b0e6560f87a78
travelersln <travelersln0@gmail.com>
2024-01-04 10:33:02 -0600

    Add

--- models.py
+++ models.py
@@ -5,7 +5,12 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:@localhost/daily_mints')
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_DB = os.getenv('MYSQL_DB')
+
+DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
 
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
