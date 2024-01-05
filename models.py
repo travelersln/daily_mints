9e013a1c1e4cd2634a43604e91192882eddd1c5b
travelersln <travelersln0@gmail.com>
2024-01-04 23:50:13 -0600

    Add

--- models.py
+++ models.py
@@ -2,15 +2,18 @@ import os
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from dotenv import load_dotenv
+load_dotenv() 
 Base = declarative_base()
 
 MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_PORT = os.getenv('MYSQL_PORT')  # Asegúrate de haber añadido esta nueva variable de entorno.
 MYSQL_DB = os.getenv('MYSQL_DB')
 
-DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?ssl_ca=C:\\Users\\alato\\Downloads\\ca.pem"
+
 
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
